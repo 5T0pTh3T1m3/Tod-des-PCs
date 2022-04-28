75b609df120d2ed15c9d9772d4fd3ee3818563c1
hannesstaude <hast1337@gmx.de>
2022-04-28 10:08:47 +0200

    zufällige bewegung der Maus

--- zufällige bewegung Maus.py
+++ zufällige bewegung Maus.py
@@ -2,13 +2,16 @@ from pynput.mouse import Controller
 import random
 import time
 import pygame
+
 pygame.init()
 mouse = Controller()
+
 #Nur für Testzwecke
 '''
 times = time.time()
 while time.time() - times < 1:
     mouse.position = (random.randint(0,pygame.display.list_modes()[0][0]), random.randint(0,pygame.display.list_modes()[0][1]))
 '''
+
 while True:
     mouse.position = (random.randint(0,pygame.display.list_modes()[0][0]), random.randint(0,pygame.display.list_modes()[0][1]))
